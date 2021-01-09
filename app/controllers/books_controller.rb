7e5980fc79bc3dfe225d07592ef51fc2ba99a3bd
panna001 <sonar.pocket.0926@gmail.com>
2021-01-09 03:16:44 +0000

    [Add] top&index

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def top
+
+  end
+
+  def new
+    @
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end

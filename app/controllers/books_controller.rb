fbd00d5f677caec900e3527695182c952608b2e6
panna001 <sonar.pocket.0926@gmail.com>
2021-01-09 08:58:25 +0000

    [Fix] create メソッド

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,17 +4,18 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = Book.new
   end
 
   def create
     book = Book.new(book_params)
     book.save
+    flash[:notice] = "Book was successfully created."
     redirect_to books_path
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -27,19 +28,23 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book)
+
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
   # 以下プライベート
   private
   def book_params
-    params[:book].permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end

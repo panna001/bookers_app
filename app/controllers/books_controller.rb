63ddb5a54d3243b025b76f3aa8b604beb463fa74
panna001 <sonar.pocket.0926@gmail.com>
2021-01-09 06:41:19 +0000

    [Add] updateメソッド

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-    
+
   def create
     book = Book.new(book_params)
     book.save
@@ -24,10 +24,16 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
   # 以下プライベート
   private
   def book_params
-    params.permit(:title, :body)
+    params[:book].permit(:title, :body)
   end
 end

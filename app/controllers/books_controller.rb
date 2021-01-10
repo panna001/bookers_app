3c42a2c5ad8fae69d1e1a44b90676a2603ae5f85
panna001 <sonar.pocket.0926@gmail.com>
2021-01-10 06:20:19 +0000

    [Add]error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,12 +4,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to books_path
     else
-      render :index
+      @books = Book.all
+      render "index"
     end
   end
 
@@ -27,8 +28,8 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(book)
     else

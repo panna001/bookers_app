6de78f2242a3f3fb8f16a424420b2d891ab363d6
panna001 <sonar.pocket.0926@gmail.com>
2021-01-10 09:22:18 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to books_path
+      redirect_to book_path(@book)
     else
       @books = Book.all
       render "index"
